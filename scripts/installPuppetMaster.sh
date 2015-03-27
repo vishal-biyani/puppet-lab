807180d82d384d03fe88b5faad3d9a1085152dbc
Vishal Biyani <vrbiyani@gmail.com>
2015-03-27 15:44:27 +0530

    Revert "initial commit of Puppet Lab"
    This reverts commit 063d44aa1d881d705cb2feab75e9b11844e15bfe.

--- scripts/installPuppetMaster.sh
+++
@@ -1,9 +0,0 @@
-sudo rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm
-sudo yum -y install puppetserver
-sudo cp /vagrant/conf/puppet.conf /etc/puppet/puppet.conf
-sudo echo "192.168.17.99 puppet.learn.com puppet puppetmaster" >> /etc/hosts
-sudo iptables -A INPUT -p tcp --dport 8140 -m state --state NEW -j ACCEPT
-sudo service iptables save
-sudo iptables -F
-sudo service iptables save
-sudo puppet master start
